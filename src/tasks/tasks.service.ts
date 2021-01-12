10f9c4ce7fd454d6f8fd6964495396dfad69cf3f
minhphan81299 <minh.phan81299@gmail.com>
2021-01-13 01:12:50 +0700

    first commit

---
+++ src/tasks/tasks.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@nestjs/common';
+import { Task, TaskStatus } from './tasks.model';
+import * as uuid from 'uuid';
+
+@Injectable()
+export class TasksService {
+  private tasks: Task[] = [];
+  getAllTasks(): Task[] {
+    return this.tasks;
+  }
+  createTask(title: string, description: string): Task {
+    const task: Task = {
+      id: uuid,
+      title,
+      description,
+      status: TaskStatus.OPEN,
+    };
+    this.tasks.push(task);
+    return task;
+  }
+}
